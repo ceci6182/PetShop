7de896564a1f0eda1d38147254f39cd50e0fc7cf
ceci6182 <ceci6182@easv365.dk>
2021-09-07 16:09:19 +0200

    Create Owner.cs

---
+++ Compulsory1.Petshop/Compulsory1.Petshop.Core/Models/Owner.cs
@@ -0,0 +1,42 @@
+namespace Compulsory1.Petshop.Domain.Models
+{
+    public class Owner
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string FirstName
+        {
+            get;
+            set;
+        }
+
+        public string LastName
+        {
+            get;
+            set;
+        }
+
+        public string Address
+        {
+            get;
+            set;
+        }
+
+        public string PhoneNumber
+        {
+            get;
+            set;
+        }
+        
+        public string Email
+        {
+            get;
+            set;
+        }
+        
+    }
+}
